2f9c8542095b5bcf5ee284d17eb03e2469223eb3
its-ak7 <v.ajay.krishnan.9@gmail.com>
2026-05-02 16:50:57 +0530

    Fix formatting issues in app.py

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def get_output(filename):
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=port)
